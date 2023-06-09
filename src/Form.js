1356543836074408e2e949eadbb949005d209d20
sameermd16 <sameerthemc@gmail.com>
2023-06-09 15:27:38 +0530

    checkbox & radio

--- src/Form.js
+++ src/Form.js
@@ -2,20 +2,84 @@ import React from 'react';
 
 function Form() {
 
-    const [name, setName] = React.useState("");
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        comments: "",
+        isFriendly: true,
+        employmentStatus: '',
+    });
+
+    console.log(formData.employmentStatus);
 
     function handleChange(event){
-        setName(event.target.value);
-        console.log(name);
+        const {name, value, type, checked} = event.target
+        setFormData((prevFormData)=> {
+            return ({
+                ...prevFormData,
+                [name] : type === 'checkbox' ? checked : value
+            })
+        })
     };
 
     return (
         <form>
             <input 
                 type='text'
-                placeholder='Enter your name'
+                placeholder='first name'
                 onChange={handleChange}
+                name='firstName'
+                value={formData.firstName}
             />
+            <input 
+                type='text'
+                placeholder='last name'
+                onChange={handleChange}
+                name='lastName'
+                value={formData.lastName}
+            />
+            <input 
+                type='email'
+                placeholder='email address'
+                onChange={handleChange}
+                name='email'
+                value={formData.email}
+            />
+            <textarea 
+                placeholder='comments'
+                onChange={handleChange}
+                name='comments'
+                value={formData.comments}
+            />
+            <input 
+                type='checkbox'
+                onChange={handleChange}
+                name='isFriendly'
+                checked={formData.isFriendly}
+                id='isFriendly'
+            />
+            <label htmlFor='isFriendly'>Are you friendly?</label>
+            <br />
+            <br />
+            <fieldset>
+                <legend>Current Employment Status</legend>
+                <input type='radio' id='unemployed' name='employmentStatus' onChange={handleChange} value='unemployed' 
+                    checked={formData.employmentStatus === "unemployed"}
+                />
+                <label htmlFor='unemployed'>Unemployed</label>
+                <br />
+                <input type='radio' id='part-time' name='employmentStatus' onChange={handleChange} value='part-time' 
+                    checked={formData.employmentStatus === "part-time"}
+                />
+                <label htmlFor='part-time'>Part-time</label>
+                <br />
+                <input type='radio' id='full-time' name='employmentStatus'onChange={handleChange} value='full-time' 
+                    checked={formData.employmentStatus === "full-time"}
+                />
+                <label htmlFor='full-time'>Full-time</label>
+                <br />
+            </fieldset>
         </form>
     )
 }
